3d52ffe60135b925acd8955ae50e85e6ea67e8a2
Lu Wang <62902203+luwangshell@users.noreply.github.com>
2024-11-23 14:03:14 +0000

    updated comments

--- comments.js
+++ comments.js
@@ -1,21 +1,8 @@
-// create web server with express and attach comments routes
-// to the web server.
-
-// Load modules
-const express = require('express');
-const comments = require('./comments');
-
-// Create an express web server
-const app = express();
-
-// Attach comments routes to the web server
-app.get('/comments', comments.list);
-app.post('/comments', comments.create);
-app.get('/comments/:id', comments.read);
-app.put('/comments/:id', comments.update);
-app.delete('/comments/:id', comments.delete);
-
-// Start the web server on port 3000
-app.listen(3000, () => {
-  console.log('Web server started on http://localhost:3000');
+// create web server Hello World app
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.end('Hello World');
+});
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
 });
