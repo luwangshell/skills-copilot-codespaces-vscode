5784347b9a1b8ac0cdb74f5b32b1a35427fe8ab9
Lu Wang <62902203+luwangshell@users.noreply.github.com>
2024-11-23 14:05:57 +0000

    updated comments

--- comments.js
+++ comments.js
@@ -3,6 +3,3 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.end('Hello World');
 });
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
-});
