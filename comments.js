40667ba0164ae437f41afc03f5615b47405c68ac
Lu Wang <62902203+luwangshell@users.noreply.github.com>
2024-11-23 14:11:24 +0000

    updated comments

--- comments.js
+++ comments.js
@@ -1,5 +1,8 @@
-// create web server Hello World app
+// Create web server Hello World app
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.end('Hello World');
+});
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
 });
